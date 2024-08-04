f893f7fd61add1b4ace23dd69a9b10451400ae19
SinghVivek1107 <viveksinghvs1107@gmail.com>
2024-08-05 01:18:25 +0530

    first commit

---
+++ src/assets/index.js
@@ -0,0 +1,17 @@
+import linkIcon from './link.svg'
+import loader from './loader.svg'
+import copy from './copy.svg'
+import logos from './logos.svg'
+import logo  from './logo.svg'
+import logoblack from './logo-black.svg'
+import tick from './tick.svg'
+
+export {
+    linkIcon,
+    loader,
+    copy,
+    logos,
+    logo,
+    logoblack,
+    tick
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,31 @@
+import { logo } from "../assets";
+
+function Hero() {
+  return (
+    <header className="w-full flex justify-center items-center flex-col">
+      <nav className="flex justify-between items-center w-full mb-10 pt-3">
+        <img src={logo} alt="summarizz" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() => window.open("https://github.com/SinghVivek1107")}
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+
+      <h1 className="head_text">
+        Rizz  Articles with <br className="max-md:hiden" />
+        <span className="orange_gradient">AI</span>
+      </h1>
+      <h2 className="desc">
+        Summarizz is your go-to tool for turning lengthy articles into concise,
+        easy-to-read summaries. Save time and get the key points quickly with
+        our advanced algorithms, perfect for students, professionals, and avid
+        readers.
+      </h2>
+    </header>
+  );
+}
+
+export default Hero;
